4d0351a631c077dd8b3de13f43b7511963cdeefa
Wendel Santos <wendel.santos@consultores.rnp.br>
2024-11-04 13:24:39 -0300

    add

--- app.py
+++ app.py
@@ -2,11 +2,13 @@ from chalice import Chalice
 
 from chalicelib.views.login import extra_routes_login
 from chalicelib.views.signup import extra_routes_signup
+from chalicelib.views.refresh_token import extra_routes_refresh_token
 
 app = Chalice(app_name='auth-ms-serverless-lambda-python')
 
 app.register_blueprint(extra_routes_signup)
 app.register_blueprint(extra_routes_login)
+app.register_blueprint(extra_routes_refresh_token)
 
 
 @app.route('/')
